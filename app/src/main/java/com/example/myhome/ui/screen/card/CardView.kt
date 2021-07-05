80d4db0e23da10e7d6af9d96fa5339e32ab31025
wkimin <wkimin@gmail.com>
2021-07-05 23:54:07 +0900

    delete warning

--- app/src/main/java/com/example/myhome/ui/screen/card/CardView.kt
+++ app/src/main/java/com/example/myhome/ui/screen/card/CardView.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
